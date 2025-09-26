4c37606dada9b68d299b6a3993e765bcc4719ae9
강정묵 <kgp294d@gmail.com>
2025-09-26 11:30:21 +0900

    환경설정

---
+++ src/main/java/com/converter/document_converter/DocumentConverterApplication.java
@@ -0,0 +1,13 @@
+package com.converter.document_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentConverterApplication.class, args);
+	}
+
+}
